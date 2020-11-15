249365025cd1aff82e4cdfa59e00c58f00e3318a
chenmaochang <chenmaochang@qq.com>
2020-11-15 23:23:45 +0800

    initial commit

---
+++ src/main/java/com/cmc/web/EBookApplication.java
@@ -0,0 +1,11 @@
+package com.cmc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EBookApplication.class, args);
+    }
+}
