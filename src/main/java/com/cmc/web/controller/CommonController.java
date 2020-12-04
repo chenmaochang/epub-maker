e9f7f402ee053f3d6808346584f348b111f0800b
chenmaochang <chenmaochang@qq.com>
2020-12-04 23:42:52 +0800

    整理漫画对应章节图片url到对应结构

--- src/main/java/com/cmc/web/controller/CommonController.java
+++ src/main/java/com/cmc/web/controller/CommonController.java
@@ -5,6 +5,7 @@ import cn.hutool.core.lang.UUID;
 import cn.hutool.http.HttpUtil;
 import com.cmc.web.beans.EBook;
 import com.cmc.web.beans.EBookChapter;
+import com.cmc.web.beans.EBookImage;
 import com.cmc.web.common.AjaxResult;
 import com.cmc.web.config.EBookConfig;
 import com.cmc.web.util.EBookGenerator;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -39,9 +41,22 @@ public class CommonController {
     }
 
     private void grabManga(String url, String startStr, String endStr) {
-        EBook eBook=EBook.builder().creator("吾峠呼世晴").title("鬼灭之刃").identifier(UUID.fastUUID().toString()).date(LocalDate.now().toString()).build();
         String html = HttpUtil.get(url);
         Document doc = Jsoup.parse(html);
+        Element titleElement = doc.selectFirst("h1");
+        String title = titleElement.text();
+        Element creatorElement = doc.selectFirst("#intro_l > div:nth-child(4) > span:nth-child(1)");
+        String creator = creatorElement.text();
+        EBook eBook = EBook.builder().creator(creator).title(title).identifier(UUID.fastUUID().toString()).date(LocalDate.now().toString()).build();
+        Element coverPicElement = doc.selectFirst(".cy_info_cover");
+        Element imgPicElement = coverPicElement.selectFirst(".pic");
+        String coverImgUrl = imgPicElement.attr("src");
+        List<EBookImage> coverImages = new LinkedList<>();
+        EBookImage coverImg = EBookImage.builder().downloadUrl(coverImgUrl).build();
+        coverImages.add(coverImg);
+        EBookChapter cover = EBookChapter.builder().images(coverImages).build();
+        eBook.setCover(cover);
+        List<EBookChapter> chapters = new LinkedList<>();
         Element element = doc.selectFirst("#mh-chapter-list-ol-0");
         List<Node> chapterNodes = element.childNodes();
         boolean touchStart = false;
@@ -52,13 +67,15 @@ public class CommonController {
                 String chapterUrl = DOMAN_SHOUMANHUA + chapterInfo.attr("href");
                 StringBuilder outHtml = new StringBuilder(chapterInfo.childNode(0).childNode(0).outerHtml());
                 outHtml.setLength(14);
-                String bookName = outHtml.toString();
-                if (bookName.equals(startStr) || touchStart) {
+                String chapterName = outHtml.toString();
+                if (chapterName.equals(startStr) || touchStart) {
                     touchStart = true;
                     String firstPage = HttpUtil.get(chapterUrl);
                     Document firstPageDoc = Jsoup.parse(firstPage);
                     Elements scripts = firstPageDoc.select("script");
-                    String base64 = null;
+                    String base64;
+                    EBookChapter chapter = EBookChapter.builder().title(chapterName).build();
+                    List<EBookImage> images = new LinkedList<>();
                     script:
                     for (int j = 0; j < scripts.size(); j++) {
                         String scriptStr = scripts.get(j).toString();
@@ -68,28 +85,28 @@ public class CommonController {
                                 String[] base64Suf = base64Pre[1].split("\";");
                                 if (base64Suf.length >= 2) {
                                     base64 = base64Suf[0];
-                                    System.out.println(base64);
-                                    String mangaUrlsStr=Base64.decodeStr(base64);
-                                    String[] mangaUrls=mangaUrlsStr.split("[$]qingtiandy[$]");
-                                    for(int k=0;k<mangaUrls.length;k++){
-                                        System.out.println(mangaUrls[k]);
+                                    String mangaUrlsStr = Base64.decodeStr(base64);
+                                    String[] mangaUrls = mangaUrlsStr.split("[$]qingtiandy[$]");
+                                    for (int k = 0; k < mangaUrls.length; k++) {
+                                        images.add(EBookImage.builder().downloadUrl(mangaUrls[k]).build());
                                     }
                                     break script;
                                 }
                             }
                         }
                     }
-
+                    chapter.setImages(images);
+                    chapters.add(chapter);
+                    System.out.println("-------done------" + chapterName);
                     if (outHtml.equals(endStr)) {
                         break;
                     }
                 }
                 continue;
-
             }
         }
-
-
+        eBook.setChapters(chapters);
+        System.out.println("--------all-----done------");
     }
 
 }
