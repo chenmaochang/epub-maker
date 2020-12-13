e18a9c66a02653be6c3a521b745eb3647d1cbb22
chenmaochang <chenmaochang@qq.com>
2020-12-13 22:00:06 +0800

    调整静态资源至图床和js cdn

--- src/main/java/com/cmc/web/util/RestTemplateUtil.java
+++ src/main/java/com/cmc/web/util/RestTemplateUtil.java
@@ -1,17 +1,61 @@
 package com.cmc.web.util;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.http.HttpUtil;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class RestTemplateUtil {
 
 
     public static void main(String[] args) {
-//        String fileUrl = "http://img001.wlbaike.com/upload2/7633/2020/10-09/20201009022936924731_small.jpg";
-//
-//        long size = HttpUtil.downloadFile(fileUrl, FileUtil.touch("/new-book/img.jpg"));
-//        System.out.println("Download size: " + size);
-        String html=HttpUtil.get("http://www.shoumanhua.com/tuili/7633/553191.html?p=1");
-        System.out.println();
+        try (final WebClient webClient = new WebClient()) {
+            webClient.setJavaScriptTimeout(10000);
+            HtmlPage page = webClient.getPage("https://www.wenshushu.cn/signin");
+            TimeUnit.SECONDS.sleep(3);
+            List<DomElement> changeMode = page.getByXPath("//*[@id=\"page_content\"]/div[1]/div[1]/ul/li[2]");
+            DomElement changeModeButton = changeMode.get(0);
+            HtmlPage loginPage = changeModeButton.click();
+            TimeUnit.SECONDS.sleep(1);
+            DomElement accountElement = loginPage.getFirstByXPath("//*[@id=\"page_content\"]/div[1]/div[2]/div[1]/div/div[1]/div[2]/input");
+            accountElement.focus();
+            accountElement.click();
+            TimeUnit.SECONDS.sleep(1);
+            loginPage.pressAccessKey('1');
+            loginPage.pressAccessKey('5');
+            loginPage.pressAccessKey('5');
+            loginPage.pressAccessKey('2');
+            loginPage.pressAccessKey('1');
+            loginPage.pressAccessKey('1');
+            loginPage.pressAccessKey('3');
+            loginPage.pressAccessKey('5');
+            loginPage.pressAccessKey('8');
+            loginPage.pressAccessKey('1');
+            loginPage.pressAccessKey('8');
+            DomElement passwordElement = loginPage.getFirstByXPath("//*[@id=\"page_content\"]/div[1]/div[2]/div[1]/div/div[2]/div[2]/input");
+            accountElement.focus();
+            passwordElement.click();
+            TimeUnit.SECONDS.sleep(1);
+            loginPage.pressAccessKey('C');
+            loginPage.pressAccessKey('H');
+            loginPage.pressAccessKey('E');
+            loginPage.pressAccessKey('N');
+            loginPage.pressAccessKey('M');
+            loginPage.pressAccessKey('A');
+            loginPage.pressAccessKey('O');
+            loginPage.pressAccessKey('C');
+            loginPage.pressAccessKey('H');
+            loginPage.pressAccessKey('A');
+            loginPage.pressAccessKey('N');
+            loginPage.pressAccessKey('G');
+            DomElement loginButtonElement = loginPage.getFirstByXPath("//*[@id=\"page_content\"]/div[1]/div[2]/div[1]/div/button");
+            HtmlPage redirectedPage=loginButtonElement.click();
+            TimeUnit.SECONDS.sleep(3);
+            System.out.println(redirectedPage.getTextContent());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
